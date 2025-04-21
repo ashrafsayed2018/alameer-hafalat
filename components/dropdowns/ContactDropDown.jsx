6258be85e112b1b57cd11adebd06a7a7f6aed4db
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2025-04-21 04:39:06 +0200

    css modifications

--- components/dropdowns/ContactDropDown.jsx
+++ components/dropdowns/ContactDropDown.jsx
@@ -1,57 +1,57 @@
-"use client";
-import Close from "../buttons/Close";
-import { useContext } from "react";
+'use client'
+import { useContext } from 'react'
+import Close from '../buttons/Close'
 
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { AppContext } from "../../context";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ContactInfo } from "../../app/data";
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { ContactInfo } from '../../app/data'
+import { AppContext } from '../../context'
 function ContactDropDown() {
-  const { contactDropIsOpen, setContactDropIsOpen } = useContext(AppContext);
+  const { contactDropIsOpen, setContactDropIsOpen } = useContext(AppContext)
   const handleClose = () => {
-    setContactDropIsOpen(false);
-  };
+    setContactDropIsOpen(false)
+  }
 
   return (
     <section
-      className={`h-screen bg-[#1a1a1a] w-full transition-transform duration-500 ease-in-out fixed top-0 left-0 z-50 ${
-        contactDropIsOpen ? "translate-y-0" : "translate-y-[-100%]"
+      className={`h-screen bg-[#1a1a1a] w-full transition-transform duration-500 ease-in-out fixed top-0 left-0 z-50 md:z-0 ${
+        contactDropIsOpen ? 'translate-y-0' : 'translate-y-[-100%]'
       }`}
     >
       <Close
-        styles={"absolute top-5 right-5 w-10 h-10 cursor-pointer"}
+        styles={'absolute top-5 right-5 w-10 h-10 cursor-pointer'}
         handleClose={handleClose}
       />
 
-      <div className="w-full h-[600px]  mx-auto flex flex-col items-center justify-center -mt-6 ">
-        <ul className="pt-[15px] mt-4 ">
+      <div className='w-full h-[600px]  mx-auto flex flex-col items-center justify-center -mt-6 '>
+        <ul className='pt-[15px] mt-4 '>
           {ContactInfo.map((link) => {
             return (
               <li
                 key={link.id}
-                className="transition-all duration-500 group mb-3"
+                className='transition-all duration-500 group mb-3'
               >
-                <a href={link.href} className="flex items-center gap-3 ">
+                <a href={link.href} className='flex items-center gap-3 '>
                   <FontAwesomeIcon
                     icon={link.icon}
-                    color="white"
-                    size="2x"
-                    className="group-hoverTextColor"
+                    color='white'
+                    size='2x'
+                    className='group-hoverTextColor'
                   />
                   <p
-                    className="text-white text-xl md:text-3xl lg:text-5xl group-hoverTextColor"
-                    dir="ltr"
+                    className='text-white text-xl md:text-3xl lg:text-5xl group-hoverTextColor'
+                    dir='ltr'
                   >
                     {link.text}
                   </p>
                 </a>
               </li>
-            );
+            )
           })}
         </ul>
       </div>
     </section>
-  );
+  )
 }
 
-export default ContactDropDown;
+export default ContactDropDown
--- components/dropdowns/MenuDropDown.jsx
+++ components/dropdowns/MenuDropDown.jsx
@@ -1,10 +1,9 @@
-"use client";
-import { useContext } from "react";
-import Close from "../buttons/Close";
-import { NavLinks } from "../../app/data";
-import Link from "next/link";
-import { AppContext } from "../../context";
-import Image from "next/image";
+'use client'
+import Image from 'next/image'
+import { useContext } from 'react'
+import { NavLinks } from '../../app/data'
+import { AppContext } from '../../context'
+import Close from '../buttons/Close'
 
 function MenuDropDown() {
   const {
@@ -16,74 +15,74 @@ function MenuDropDown() {
     setShareDropIsOpen,
     contactDropIsOpen,
     setContactDropIsOpen,
-  } = useContext(AppContext);
+  } = useContext(AppContext)
 
   const handleClose = () => {
-    setMenuIsOpen(false);
-  };
+    setMenuIsOpen(false)
+  }
 
   return (
     <section
-      className={`h-screen bg-[#1a1a1a] w-full transition-transform duration-500 ease-in-out fixed top-0 left-0 z-50 ${
-        menuIsOpen ? "translate-y-0" : "translate-y-[-100%]"
+      className={`h-screen bg-[#1a1a1a] w-full transition-transform duration-500 ease-in-out fixed top-0 left-0 z-50 md:z-0 ${
+        menuIsOpen ? 'translate-y-0' : 'translate-y-[-100%]'
       }`}
     >
       <Close
-        styles={"absolute top-5 right-5 w-10 h-10 cursor-pointer"}
+        styles={'absolute top-5 right-5 w-10 h-10 cursor-pointer'}
         handleClose={handleClose}
       />
       {/* navlinks */}
-      <ul className="flex flex-col items-center justify-center gap-5 pt-44">
+      <ul className='flex flex-col items-center justify-center gap-5 pt-44'>
         {NavLinks.map((navLink) => {
           return (
-            <li key={navLink.id} className="text-white">
+            <li key={navLink.id} className='text-white'>
               <a
                 href={navLink.url}
-                className="text-xl hoverTextColor cursor-pointer"
+                className='text-xl hoverTextColor cursor-pointer'
               >
                 {navLink.name}
               </a>
             </li>
-          );
+          )
         })}
       </ul>
 
-      <div className="w-24 h-[.7px] themeBgColor mx-auto mt-[120px] flex items-start justify-center ">
-        <ul className="flex items-center gap-1 pt-[15px]">
-          <li className="bg-white cursor-pointer w-[29px] h-[29px] rounded-full flex items-center justify-center">
+      <div className='w-24 h-[.7px] themeBgColor mx-auto mt-[120px] flex items-start justify-center '>
+        <ul className='flex items-center gap-1 pt-[15px]'>
+          <li className='bg-white cursor-pointer w-[29px] h-[29px] rounded-full flex items-center justify-center'>
             <Image
-              src="/icons/location-arrow.svg"
-              alt="location"
+              src='/icons/location-arrow.svg'
+              alt='location'
               width={12}
               height={12}
-              className="h-3 w-3"
+              className='h-3 w-3'
               onClick={() => setMapDropIsOpen(!mapDropIsOpen)}
             />
           </li>
-          <li className="bg-white cursor-pointer w-[29px] h-[29px] rounded-full flex items-center justify-center">
+          <li className='bg-white cursor-pointer w-[29px] h-[29px] rounded-full flex items-center justify-center'>
             <Image
-              src="/icons/share-alt-black.svg"
-              alt="share"
+              src='/icons/share-alt-black.svg'
+              alt='share'
               width={12}
               height={12}
-              className="h-3 w-3"
+              className='h-3 w-3'
               onClick={() => setShareDropIsOpen(!shareDropIsOpen)}
             />
           </li>
-          <li className="bg-white cursor-pointer w-[29px] h-[29px] rounded-full flex items-center justify-center">
+          <li className='bg-white cursor-pointer w-[29px] h-[29px] rounded-full flex items-center justify-center'>
             <Image
-              src="/icons/phone-black.svg"
-              alt="phone"
+              src='/icons/phone-black.svg'
+              alt='phone'
               width={12}
               height={12}
-              className="h-3 w-3"
+              className='h-3 w-3'
               onClick={() => setContactDropIsOpen(!contactDropIsOpen)}
             />
           </li>
         </ul>
       </div>
     </section>
-  );
+  )
 }
 
-export default MenuDropDown;
+export default MenuDropDown

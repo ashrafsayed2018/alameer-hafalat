08889986089fe42d4051e55b3ff4952eb4f48493
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2025-04-21 04:30:17 +0200

    add article images

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -8,22 +8,23 @@ const images = Array.from(
 
 export default function Gallery() {
   return (
-    <div className="mt-28 px-4 md:px-6 lg:px-8 xl:px-10">
-      <h2 className="headingColor">صور من اعمالنا</h2>
-      <span className="headingBorderColor"></span>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className='mt-28 px-4 md:px-6 lg:px-8 xl:px-10'>
+      <h2 className='headingColor'>صور من اعمالنا</h2>
+      <span className='headingBorderColor'></span>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
         {ServicesList.map((service, index) => {
           return (
             <div
               key={index}
-              className="relative h-[600px] overflow-hidden rounded-lg shadow-lg transform transition duration-300 ease-in-out hover:rotate-3 hover:shadow-xl"
+              className='relative h-[600px] overflow-hidden rounded-lg shadow-lg transform transition duration-300 ease-in-out hover:rotate-3 hover:shadow-xl'
             >
               <Image
                 src={service.image}
                 alt={`Image ${index + 1}`}
-                layout="fill" // Fills the entire parent container
-                objectFit="cover" // Ensures the image covers the container
-                className="transition duration-300 transform hover:scale-110"
+                width={500}
+                height={500}
+                objectFit='cover' // Ensures the image covers the container
+                className='transition duration-300 transform hover:scale-110'
               />
             </div>
           )
